024cf5624684a16ee939ee9aeef4a2d9f11b1b28
Paul Kramme <pjkramme@gmail.com>
2016-09-19 18:54:00 +0200

    Add arg iterator

--- lin-client/src/main.c
+++ lin-client/src/main.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+	int argiterator = 0;
+	for(argiterator = 0; argiterator < argc; argiterator++)
+	{
+		if(strcmp(argv[argiterator], "version") == 0)
+		{
+			puts("This should display version");
+		}
+		else if(strcmp(argv[argiterator], "new") == 0)
+		{
+			puts("This should create a new block");
+		}
+		else
+		{
+			printf("Command '%s' not regognized\n", argv[argiterator]);
+		}
+	}
 	puts("Linux Backup Tool");
 	return 0;
 }
