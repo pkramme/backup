7278305208078a1ee9830b617d3aecf73dec351f
Paul Kramme <pjkramme@gmail.com>
2016-09-19 20:37:45 +0200

    Add new name regognition

--- lin-client/src/main.c
+++ lin-client/src/main.c
@@ -1,9 +1,11 @@
+static char startmessage[] = "Linux Backup Client";
 #include<stdio.h>
 #include<string.h>
 
 int main(int argc, char *argv[])
 {
 	int argiterator = 0;
+	puts(startmessage);
 	for(argiterator = 0; argiterator < argc; argiterator++)
 	{
 		if(strcmp(argv[argiterator], "version") == 0)
@@ -13,13 +15,13 @@ int main(int argc, char *argv[])
 		else if(strcmp(argv[argiterator], "new") == 0)
 		{
 			puts("This should create a new block");
+			printf("Creating new block with name '%s'\n", argv[argiterator + 1]);
 		}
 		else
 		{
 			printf("Command '%s' not regognized\n", argv[argiterator]);
 		}
 	}
-	puts("Linux Backup Tool");
 	return 0;
 }
 
