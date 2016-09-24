d44c8813e0d7f001c26c4097eba0523c84c0ec7f
Paul Kramme <pjkramme@gmail.com>
2016-09-24 17:19:17 +0200

    Add new create function

--- lin-client/src/db.c
+++ lin-client/src/db.c
@@ -16,3 +16,16 @@ int write_database(char write[256])
 	fclose(database);
 	return 0;
 }
+
+int create_database_new(char path[256])
+{
+	int fd;
+	/*int creat(char *name, int perms);*/
+	if(fd = creat("btsootdb", "777") == -1)
+	{
+		puts("DATABASE CREATION FAIL.");
+		return 1;
+	}
+	return 0;
+}	
+	
--- lin-client/src/db.h
+++ lin-client/src/db.h
@@ -2,7 +2,7 @@
 #define DB_H_INCLUDED
 
 #include<stdio.h>
-
+#include<fcntl.h>
 int create_database(char path[256]);
 int write_database(char write[256]);
 
