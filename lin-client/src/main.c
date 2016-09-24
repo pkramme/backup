44df380c3b70c9db3fb74c7b5f3dc3eda90637a6
Paul Kramme <pjkramme@gmail.com>
2016-09-24 11:41:12 +0200

    Add partial installation walkthrough

--- lin-client/src/main.c
+++ lin-client/src/main.c
@@ -2,38 +2,66 @@ static char version[] = "btsoot 0.1.0";
 static char startmessage[] = "Linux Backup Client";
 #include<stdio.h>
 #include<string.h>
+#include<unistd.h>
+
 #include"db.h"
+#include"getch/getch.h"
 
 int main(int argc, char **argv)
 {
 	int argindex = 0;
+	int create_conf_file = 0;
 	printf("%s", startmessage);
-	for(argindex = 1; argindex < argc; argindex++)
+	if(argc > 1)
 	{
-		if(strcmp(argv[argindex], "version") == 0)
+		for(argindex = 1; argindex < argc; argindex++)
 		{
-			printf("%s", version);
-		}
-		else if(strcmp(argv[argindex], "new") == 0)
-		{
-			puts("This should create a new block");
-			printf("Creating new block with name '%s'\n", argv[argindex + 1]);
-			++argindex;
-			write_database(argv[argindex + 1]);
-		}
-		else if(strcmp(argv[argindex], "create") == 0)
-		{
-			if(create_database(argv[argindex + 1]))
+			if(strcmp(argv[argindex], "version") == 0)
 			{
-				puts("Cannot open file.");
+				printf("%s", version);
 			}
-			++argindex;
-		}
-		else
-		{
-			printf("Command '%s' not regognized\n", argv[argindex]);
+			else if(strcmp(argv[argindex], "new") == 0)
+			{
+				puts("This should create a new block");
+				printf("Creating new block with name '%s'\n", argv[argindex + 1]);
+				++argindex;
+				write_database(argv[argindex + 1]);
+			}
+			else if(strcmp(argv[argindex], "create") == 0)
+			{
+				if(create_database(argv[argindex + 1]))
+				{
+					puts("Cannot open file.");
+				}
+				++argindex;
+			}
+			else
+			{
+				printf("Command '%s' not regognized\n", argv[argindex]);
+			}
 		}
 	}
+	puts("You started BTSOOT without arguments.");
+	if(access("btsoot.conf", F_OK) != -1)
+	{
+		puts("Configfile found!");
+	}
+	else
+	{
+		puts("It seems like this is the first start.");
+		puts("Shall i create a config file? (Y/n)");
+		create_conf_file = getch();
+		switch(create_conf_file)
+		{
+			case 'n':
+				puts("I will NOT create a config file.");
+				puts("You should create one by yourself.");
+				return 1;
+			default:
+				puts("Proceeding to create config file.");
+				/*config file creator.*/
+		}
+	}	
 	return 0;
 }
 
