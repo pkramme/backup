1f64d7357a795826184c16527f66b08a4b5355bd
Paul Kramme <pjkramme@gmail.com>
2016-09-28 14:38:12 +0200

    Solve ISO89 violation warning

--- lin-client/src/configfile.c
+++ lin-client/src/configfile.c
@@ -2,8 +2,8 @@
 
 int create_configfile(char path[256])
 {
-	puts("Creating to create config.");
 	FILE *config_file;
+	puts("Creating to create config.");
 	config_file = fopen("btsoot.conf", "w");
 	fprintf(config_file, "Testtestetetstettsst\n");
 	fclose(config_file);
